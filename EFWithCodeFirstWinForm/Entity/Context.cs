4c2f6ce0b90aed6912883f20d577576ba2b8c5b7
Kadir Hali <kadir.hali@iksirsoft.com>
2023-07-17 12:27:32 +0300

    Add project files.

---
+++ EFWithCodeFirstWinForm/Entity/Context.cs
@@ -0,0 +1,9 @@
+﻿using System.Data.Entity;
+
+namespace EFWithCodeFirstWinForm.Entity
+{
+    public class Context : DbContext
+    {
+        public DbSet<Urunler> Urunlers { get; set; }
+    }
+}
---
+++ EFWithCodeFirstWinForm/Entity/Urunler.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EFWithCodeFirstWinForm.Entity
+{
+    public class Urunler
+    {
+        [Key]
+        public int UrunID { get; set; }
+        public string UrunAd { get; set; }
+        public string UrunMarka { get; set;}
+        public string UrunKategori { get; set; }
+        public int UrunStok { get; set; }
+    }
+}
